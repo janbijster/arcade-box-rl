3aa56592ea187654e8405b50659534ddc7d63f68
Jan Bijster <janbijster@gmail.com>
2018-10-08 09:14:26 +0200

    fullscreen

--- src/Environment.js
+++ src/Environment.js
@@ -8,7 +8,9 @@ class Environment {
     this.engine = MATTER.Engine.create()
 
     // create a renderer
+    this.canvas = document.getElementById('canvas');
     this.render = MATTER.Render.create({
+        canvas: this.canvas,
         element: document.body,
         engine: this.engine
     });
@@ -17,6 +19,18 @@ class Environment {
     const boxA = MATTER.Bodies.rectangle(400, 200, 80, 80);
     const boxB = MATTER.Bodies.rectangle(450, 50, 80, 80);
     const ground = MATTER.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    this.engine.world.bounds.max.x = window.width;
+    this.engine.world.bounds.max.y = window.height;
+
+    function resizeCanvas() {
+      document.getElementById('canvas').width = window.innerWidth;
+      document.getElementById('canvas').height = window.innerHeight;
+    }
+    resizeCanvas();
+
+    window.addEventListener('resize', resizeCanvas, false);
+    //resizeCanvas();
 
     // add all of the bodies to the world
     MATTER.World.add(this.engine.world, [boxA, boxB, ground]);
